c51d85f100e31b38fd81951db4bd99690048178c
FoodForeCode <99714211+FoodForeCode@users.noreply.github.com>
2022-02-15 10:02:01 +0300

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "Tebriks! Bir Sonraki Aşamaya Geçtin. Bir Sonraki İpucu Önceki Hediyede Saklı" : "Game over!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
